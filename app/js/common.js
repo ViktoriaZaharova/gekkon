3b8f9964806b92d58665fd499db729c350fef2e9
viktoriazaharova <zaharova181114@gmail.com>
2020-12-04 01:52:16 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,73 @@
+$('.main-slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    fade: true
+});
+
+$('.masters-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    fade: true
+});
+
+$('.reviews-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('.licenses-slider').slick({
+    slidesToShow: 4,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+});
+
+$('.go_to').click(function () {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    $('.mobile-menu').fadeOut();
+    return false;
+});
+
+$('.btn-burger').click(function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').click(function () {
+    $('.mobile-menu').fadeOut();
+});
